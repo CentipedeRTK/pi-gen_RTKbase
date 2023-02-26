fa0cdfa97a43f5498cec7f3d03fc5490bc94ab96
ANCELIN Julien <julien.ancelin@inra.fr>
2023-02-26 11:57:35 +0100

    Update note_JA.sh

--- note_JA.sh
+++ note_JA.sh
@@ -1,5 +1,8 @@
 #http://kmdouglass.github.io/posts/create-a-custom-raspbian-image-with-pi-gen-part-1/
 
+Installing qemu-user-static
+sudo apt-get install qemu-user-static
+
 #make executable .sh
 cd pi-gen
 sudo find ./ -type f -iname "*.sh" -exec chmod +x {} \;
@@ -8,20 +11,19 @@ sudo find ./ -type f -iname "*.sh" -exec chmod +x {} \;
 touch ./stage4/SKIP ./stage5/SKIP
 touch ./stage4/SKIP_IMAGES ./stage5/SKIP_IMAGES
 
-
 docker rm -v pigen_work
 
 #apt_proxy docker-compose up -d
 docker-compose up -d
 
-sudo ./build-docker.sh 
-sudo CONTINUE=1 ./build-docker.sh
+./build-docker.sh 
+CONTINUE=1 ./build-docker.sh
 
-sudo PRESERVE_CONTAINER=1 ./build-docker.sh
+PRESERVE_CONTAINER=1 ./build-docker.sh
 
-sudo PRESERVE_CONTAINER=1 CONTINUE=1 ./build-docker.sh
+PRESERVE_CONTAINER=1 CONTINUE=1 ./build-docker.sh
 
-sudo PRESERVE_CONTAINER=1 CONTINUE=1 CLEAN=1 ./build-docker.sh
+PRESERVE_CONTAINER=1 CONTINUE=1 CLEAN=1 ./build-docker.sh
 
 
-sudo docker run -it --privileged --volumes-from=pigen_work pi-gen /bin/bash
+docker run -it --privileged --volumes-from=pigen_work pi-gen /bin/bash
