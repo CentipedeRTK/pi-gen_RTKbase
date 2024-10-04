ba1dd73db174a4f7025557190297dca4df740579
jancelin <julien.ancelin@inra.fr>
2024-10-04 18:57:28 +0000

    resolv bug ntripA disable after reboot

--- stage_rtkbase/00-rtkbase/files/firstboot.sh
+++ stage_rtkbase/00-rtkbase/files/firstboot.sh
@@ -2,3 +2,5 @@
 
 cd /home/basegnss/rtkbase/tools && \
 sudo ./install.sh --user basegnss --unit-files --detect-modem --detect-gnss --configure-gnss --start-services
+sudo rm /lib/systemd/system/firstboot.service
+
